5bbebe6f819132fab034f8438b2d6428b008ce43
raulzc3 <raulzc3pc@gmail.com>
2020-12-22 19:57:33 +0100

    Subo ejercicio createPhoneNumbers.js

---
+++ 6kyu/createPhoneNumber.js
@@ -0,0 +1,22 @@
+/*
+url: https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
+Write a function that accepts an array of 10 integers (between 0 and 9), 
+that returns a string of those numbers in the form of a phone number.
+Example: 
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+*/
+
+function createPhoneNumber(numbers) {
+  let result = "(";
+  for (let i = 0; i < 10; i++) {
+    result += numbers[i];
+    if (i === 2) {
+      result += ") ";
+    } else if (i === 5) {
+      result += "-";
+    }
+  }
+  return result;
+}
